18ca1c58ab02b2d21822590ef35b221fca9e58ad
Phuc La <phuc.la@asnet.com.vn>
2021-01-18 14:05:40 +0700

    Create structure for libs

--- chanel-store/jest.config.js
+++ chanel-store/jest.config.js
@@ -4,5 +4,8 @@ module.exports = {
     '<rootDir>/apps/store-manager-portal',
     '<rootDir>/apps/customer-portal',
     '<rootDir>/libs/core',
+    '<rootDir>/libs/auth',
+    '<rootDir>/libs/product',
+    '<rootDir>/libs/customer',
   ],
 };

c553f604f9bce0ca2ce7025d737945c6362184b4
Phuc La <phuc.la@asnet.com.vn>
2021-02-02 17:06:36 +0700

    Update api global prefix

--- chanel-store/apps/admin-portal/src/main.ts
+++ chanel-store/apps/admin-portal/src/main.ts
@@ -12,6 +12,8 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
   const options = new DocumentBuilder()
     .addBearerAuth()
     .setTitle('Admin portal API')
@@ -24,8 +26,6 @@ async function bootstrap() {
   });
   SwaggerModule.setup('api/doc', app, document);
 
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);

647246b0ab67cd7bd479779dfc4c3948ae9b8b5c
Phuc La <phuc.la@asnet.com.vn>
2021-02-18 15:13:24 +0700

    Create store service

---
+++ chanel-store/libs/store/src/store.dto.ts
@@ -0,0 +1,20 @@
+// Standard library
+// External module
+import { filterMetadata } from '@chanel-store/core';
+
+// Internal module
+import { Store, BusinessHour } from './store.entity';
+
+export class CreateStoreDTO extends filterMetadata(
+  Store,
+  'swagger/apiModelPropertiesArray',
+  [':id', ':created_at', ':updated_at', ':deleted_at'],
+  'CreateStoreDTO'
+) {}
+
+export class CreateBusinessHourDTO extends filterMetadata(
+  BusinessHour,
+  'swagger/apiModelPropertiesArray',
+  [':id', ':created_at', ':updated_at', ':deleted_at'],
+  'CreateBusinessHourDTO'
+) {}

df2937e97ac6a9e69e2987d561d5a1223d3165f1
Phuc La <phuc.la@asnet.com.vn>
2021-02-25 15:52:23 +0700

    Add moment js module

--- chanel-store/libs/core/src/interceptor/logging.interceptor.ts
+++ chanel-store/libs/core/src/interceptor/logging.interceptor.ts
@@ -7,17 +7,18 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import * as moment from 'moment';
 
 @Injectable()
-export class LoggingInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+export class LoggingInterceptor<T> implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<T> {
     const logger = new Logger();
     logger.log('Before...');
     const request = context.switchToHttp().getRequest();
-    const now = Date.now();
+    const now = moment();
     return next.handle().pipe(
       tap(() => {
-        logger.log(`${request.method} ${request.url} ${Date.now() - now}ms`);
+        logger.log(`${request.method} ${request.url} ${moment().diff(now)}ms`);
       })
     );
   }
